644f4eb9264717ef3e03c6d06e76e08fd41b34f5
Hugo Hua <hkh4@williams.edu>
2022-05-02 21:32:37 -0400

    setup

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+app.use(express.static('client/build'))
+
+// This will be used when deploying to heroku
+app.get("*", (req,res) => {
+	res.sendFile(path.join(__dirname, "client/build", "index.html"))
+})
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log("Running on port 5000");
+})
