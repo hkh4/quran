705068195f6776514a9cf4bfbde8d02bd869c469
Hugo Hua <hkh4@williams.edu>
2022-05-05 17:49:34 -0400

    surah

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ function App() {
 
   const imageRef = useRef()
 
+  const verseRefs = useRef([])
+  const verseNumberRefs = useRef([])
+
   function scroll(e) {
     imageRef.current.style.backgroundPosition = "50% " + window.pageYOffset * 0.7 + "px"
   }
@@ -19,6 +22,63 @@ function App() {
     }
   }, [])
 
+
+
+
+  const verseVisible = (entries) => {
+
+    const [entry] = entries
+    const target = entry.target
+    const id = parseInt(entry.target.id)
+
+    if (entry.isIntersecting) {
+      target.classList.add("active")
+      verseNumberRefs.current[id].classList.add("active")
+
+
+      // if (verseRefs.current) {
+      //   if (id >= 1) {
+      //     verseRefs.current[id - 1].classList.remove("active")
+      //   }
+      //   if (id <= verseRefs.current.length - 2) {
+      //     verseRefs.current[id + 1].classList.remove("active")
+      //   }
+      //
+      // }
+
+    } else {
+      target.classList.remove("active")
+      verseNumberRefs.current[id].classList.remove("active")
+    }
+
+  }
+
+  useEffect(() => {
+
+    const options = {
+      rootMargin: "-49.5% 0% -49.5% 0%"
+    }
+
+    const current = verseRefs.current
+
+    const observer = new IntersectionObserver(verseVisible, options)
+
+    if (current) {
+      for (const element of current) {
+        observer.observe(element)
+      }
+    }
+
+
+    return () => {
+      for (const element of current) {
+        observer.unobserve(element)
+      }
+    }
+
+  }, [verseRefs])
+
+
   return (
 
     <div>
@@ -30,21 +90,37 @@ function App() {
 
       <div className="main">
 
-        <div className="sidebar"></div>
+        <div className="sidebar">
+
+          <div className="verse-numbers">
+
+            {
+              verses.map((el, index) => {
+                return <span ref={e => verseNumberRefs.current[index] = e} key={index} id={`verse-${index}`}>{`Verse ${index}`}</span>
+              })
+            }
+
+          </div>
+
+        </div>
 
         <div className="surah">
 
           {
             verses.map((el, index) => {
               return (
-                <div className="verse" key={index} id={index}>
-                  <span>{el}</span>
-                </div>
+                <span className="verse" id={index} key={index} ref={e => verseRefs.current[index] = e}>{el}</span>
               )
             })
           }
 
         </div>
+
+      </div>
+
+      <div className="footer">
+
+        <span>&#169; Hugo Hua</span>
 
       </div>
 
